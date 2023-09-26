44790cd9d849d5ec85f572c2220454d2938e5be4
Gabriel Portela <gabriel.portela@ime.eb.br>
2023-09-26 16:48:37 -0400

    A2 feito

--- A2.py
+++ A2.py
@@ -2,24 +2,15 @@ respostas = open("A2_answer.txt", "w")
 
 for i in range(int(input())):
     a, b, c = [int(x) for x in input().split()]
+    simples = [0, 1, 2, c//a]
     valores = []
-    duplo = c//b
-    simples = (c - duplo*b)//a
-    tem_pao = 2*(simples+duplo)
-    tem_recheio = simples + 2*duplo
-    if(tem_pao < 2 or tem_recheio < 1):
-        resposta = 0
-    else:
-        resposta = min(tem_recheio, tem_pao-1)
-    valores.append(resposta)
-    simples = c//a
-    duplo = (c - simples*a)//b
-    tem_pao = 2*(simples+duplo)
-    tem_recheio = simples + 2*duplo
-    if(tem_pao < 2 or tem_recheio < 1):
-        resposta = 0
-    else:
-        resposta = min(tem_recheio, tem_pao-1)
-    valores.append(resposta)
+    for alternativa in simples:
+        duplo = (c - alternativa*a)//b
+        tem_pao = 2*(alternativa+duplo)
+        tem_recheio = 2*duplo + alternativa
+        if(tem_pao < 2 or tem_recheio < 1):
+            valores.append(0)
+        else:
+            valores.append(min(tem_pao-1, tem_recheio))
     print(f'Case #{i+1}: {max(valores)}')
     respostas.write(f'Case #{i+1}: {max(valores)}' + '\n')
